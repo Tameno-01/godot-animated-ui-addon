41274b1bfdd6466a3066284045c2639be305816d
Tameno-01 <tamenorias@gmail.com>
2025-02-24 19:20:16 +0100

    Begin work

---
+++ addons/animated_ui/ui_aimation.gd
@@ -0,0 +1,6 @@
+class_name UiAnimation
+extends Resource
+
+
+func animate(time: float, context: UiAnimationContext) -> UiAnimatedProperties:
+	return UiAnimatedProperties.new()
---
+++ addons/animated_ui/ui_animation_context.gd
@@ -0,0 +1,2 @@
+class_name UiAnimationContext
+extends RefCounted
